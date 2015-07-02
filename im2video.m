d2396905639385681ceb5fb2aa328ffc9289a29f
Conan <lrjconan@gmail.com>
2015-07-02 17:13:01 +0800

    Update im2video.m

--- im2video.m
+++ im2video.m
@@ -1,14 +1,14 @@
 function im2video(img_path, img_fmt, save_name, frame_rate)
 
-frame_names         = dir([img_path '\*.' img_fmt]);
+frame_names         = dir(fullfile(img_path, sprintf('*.%s', img_fmt)));
 num_frames          = numel(frame_names);
-writerObj   		    = VideoWriter([save_name '.avi']);
+writerObj           = VideoWriter(save_name);
 writerObj.FrameRate = frame_rate;
 
 open(writerObj);
 
 for i = 1 : num_frames
-    img = im2double(imread([img_path frame_names(i, 1).name]));
+    img = im2double(imread(fullfile(img_path, frame_names(i, 1).name)));
     writeVideo(writerObj, img);
 end
 
