49dac9d1436cb74e4c15ad29a6efb3994b8123f8
Conan <lrjconan@users.noreply.github.com>
2016-04-24 19:01:03 -0400

    Update MNIST_resize.m
    bug related to resize is fixed

--- MNIST_resize.m
+++ MNIST_resize.m
@@ -1,6 +1,6 @@
 function [img_out] = MNIST_resize(img, long_border_roi_resize, height_out, width_out)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Perform a MNIST style resize for simple images (e.g., binary digits)
+% Perform MNIST style resize for simple images (e.g., binary digits)
 % Input:
 %       img: input image, should be gray scale (background = 0, ROI > 0)
 %       long_border_roi_resize: long-border size of ROI after resized
@@ -19,26 +19,47 @@ if long_border_roi_resize >= max(height_out, width_out)
     error('Long-border of ROI should not exceed the long-border of image!');
 end
 
+if ndims(img) ~= 2
+    error('Input image should be gray scale!');
+end
+
+[height, width] = size(img);
 [row_img, col_img] = find(img > 0);
-long_border = max(max(row_img) - min(row_img) + 1, max(col_img) - min(col_img) + 1);
-resize_ratio = long_border_roi_resize / long_border;
-img_small = imresize(img, resize_ratio);
 
-% centerized image
-[row_img_small, col_img_small] = find(img_small > 0);
-x0 = min(col_img_small);
-y0 = min(row_img_small);
-roi_height = max(row_img_small) - y0 + 1;
-roi_width  = max(col_img_small) - x0 + 1;
+% compute ROI coordinates
+x0 = min(col_img);
+y0 = min(row_img);
+roi_height = max(row_img) - y0 + 1;
+roi_width  = max(col_img) - x0 + 1;
 
-[py, px] = ndgrid(1:size(img_small,1), 1:size(img_small,2));            
-center_x = round(mean(px(logical(img_small))));
-center_y = round(mean(py(logical(img_small))));
+% compute center of mass
+[py, px] = ndgrid(1:height, 1:width);
+center_x = round(mean(px(logical(img))));
+center_y = round(mean(py(logical(img))));
+
+% shift image
+img_center = zeros(size(img));
 dx = center_x - x0 + 1;
 dy = center_y - y0 + 1;
+x = max(floor(width / 2) - dx, 1);
+y = max(floor(height / 2) - dy, 1);
+img_center(y : y + roi_height - 1, x : x + roi_width - 1) =  ...
+    img(y0 : y0 + roi_height - 1, x0 : x0 + roi_width - 1 );
 
+% resize image
+long_border = max(roi_height, roi_width);
+resize_ratio = long_border_roi_resize / long_border;
+img_small = imresize(img_center, resize_ratio);
+
+% shift to the output grid
 img_out = zeros(height_out, width_out);
-x = max(floor(width_out / 2) - dx, 1);
-y = max(floor(height_out / 2) - dy, 1);
-img_out(y : y + roi_height - 1, x : x + roi_width - 1) =  ...
-    img_small(y0 : y0 + roi_height - 1, x0 : x0 + roi_width - 1 );
+[height, width] = size(img_small);
+roi_height_small = floor(roi_height * resize_ratio);
+roi_width_small = floor(roi_width * resize_ratio);
+y0 = max(floor(height / 2 - roi_height_small / 2), 1);
+x0 = max(floor(width / 2 - roi_width_small / 2), 1);
+y = max(floor(height_out / 2 - roi_height_small / 2), 1);
+x = max(floor(width_out / 2 - roi_width_small / 2), 1);
+
+img_out(y : y + roi_height_small - 1, x : x + roi_width_small - 1) =  ...
+    img_small(y0 : y0 + roi_height_small - 1, x0 : x0 + roi_width_small - 1 );
